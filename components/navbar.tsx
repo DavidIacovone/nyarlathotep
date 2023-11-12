9677ba876bca7a744a0a5b31ab997b8c3f2a7c2c
DavidIacovone <iacovone.david@gmail.com>
2023-11-12 21:18:33 +0100

    Updated Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,6 +6,7 @@ import { Poppins } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { UserButton } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
+import { ModeToggle } from '@/components/mode-toggle';
 
 const font = Poppins({
 	weight: '600',
@@ -36,6 +37,7 @@ export const Navbar = () => {
 					Upgrade
 					<Sparkles className='h-4 w-4 fill-white text-white ml-2' />
 				</Button>
+				<ModeToggle />
 				<UserButton />
 			</div>
 		</div>
