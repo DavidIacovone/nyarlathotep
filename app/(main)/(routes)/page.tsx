a284eec82c15880b870e58c47eefccfa3093f936
DavidIacovone <iacovone.david@gmail.com>
2023-11-12 22:18:23 +0100

    Added searching feature to the main page

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,12 @@
+import { SearchInput } from '@/components/search-input';
 import { UserButton } from '@clerk/nextjs';
 
 const MainPage = () => {
-	return <div></div>;
+	return (
+		<div className='h-full p-4 space-y-2'>
+			<SearchInput />
+		</div>
+	);
 };
 
 export default MainPage;
