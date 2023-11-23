4631a42d7fcb21553d75d129de44e6bdbf9c8671
DavidIacovone <iacovone.david@gmail.com>
2023-11-23 21:36:53 +0100

    Updated app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
 import { ThemeProvider } from '@/components/theme-provider';
+import { ProModal } from '@/components/pro-modal';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 						attribute='class'
 						defaultTheme='system'
 						enableSystem>
+						<ProModal />
 						{children}
 					</ThemeProvider>
 				</body>
